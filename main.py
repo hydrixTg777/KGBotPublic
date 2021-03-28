d4e75d69524c66741871b297181ae4676da85f8b
Laimusp <Armis3005@gmail.com>
2021-03-28 22:53:34 +0300

    add secret and del config

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 import os
 
-app = Client("my_account")
+app = Client("account", api_id = 6, api_hash = "eb06d4abfb49dc3eeb1aeb98ae0f581e", plugins = dict(root = "modules"))
 
 app.run()
 	
